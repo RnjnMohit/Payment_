cfbba1c4cb6107db6cc98de91d5847e827955113
MohitRanjan2433 <mohitranjan11082@gmail.com>
2024-01-14 22:39:35 +0530

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Info from './components/InformationSection/Info'
 import Navbar from './components/Navbar_setup/Navbar'
+import Frame from './pages/login';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Info />
+      <Frame/>
       {/* d */}
     </>
   )
