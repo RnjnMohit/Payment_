3b6ad818de9c52863502448c78e0b80bd3d2659a
MohitRanjan2433 <mohitranjan11082@gmail.com>
2024-01-14 22:27:18 +0530

    d

--- src/components/Navbar_setup/Navbar.jsx
+++ src/components/Navbar_setup/Navbar.jsx
@@ -6,10 +6,10 @@ export default function Navbar(){
         <div className="flex justify-between align-middle h-28 border-b border-blue-800">
             <p className=" m-10">LOGO</p>
             <div>
-                <NavbarButton name="Personal" />
+                {/* <NavbarButton name="Personal" />
                 <NavbarButton name="Buisness" />
                 <NavbarButton name="Company" />
-                <NavbarButton name="Help" />
+                <NavbarButton name="Help" /> */}
 
                 
                 <NavbarButton name="Login"  custom="ml-28"/>
