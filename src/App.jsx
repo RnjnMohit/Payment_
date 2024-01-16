988874ab0dd8ea8f210377147332fbf778cf2c65
MohitRanjan2433 <mohitranjan11082@gmail.com>
2024-01-16 18:34:58 +0530

    cdc

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-import './App.css'
-import Signup from './pages/signup';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import { set } from 'rsuite/esm/utils/dateUtils';
-import Login from './pages/Login';
-import Profilepage from './components/Profilepage/Profilepage';
-=======
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import { useState } from 'react'
-<<<<<<< HEAD
-import Navbar from './components/Navbar_setup/Navbar'
-=======
 import Navbar from './components/Navbar_setup/Navbar';
->>>>>>> 91aa5f79754157a3e0b7e1da2d22d54e4075642e
->>>>>>> 039846900d63bf693911f345390124362604c743
 
 
 function App() {
@@ -28,16 +13,7 @@ function App() {
   return (
     <>
 
-<<<<<<< HEAD
-      {/* <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn}/>}/>
-        <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn}/>}/>
-      </Routes> */}
-      <Profilepage/>
-=======
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}  />
->>>>>>> 91aa5f79754157a3e0b7e1da2d22d54e4075642e
 
       <Routes>
         <Route path='/' element={<Home />} /> 
