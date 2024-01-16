27046d9019dbfd3ba855ef2e1b7086933cd0f7a1
PranavGaur7 <raghavgau7@gmail.com>
2024-01-16 19:51:03 +0530

    profile featue added

--- src/components/Profilepage/User.jsx
+++ src/components/Profilepage/User.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './user.css'
 const User = () => {
+    const [disable, setDisable] = useState(true);
+    let originalData = {
+        name: "Pranav Gaur",
+        email: "gasjdaj@gmail.com",
+        phone: "67217313983"
+    }
+    const [data, setData] = useState(originalData);
+    const onEdit = (e) => {
+        e.preventDefault();
+        setDisable(!disable);
+        if (document.querySelector('.editbtn').innerHTML === "Edit") {
+            document.querySelector('.editbtn').innerHTML = "Cancel";
+            document.querySelector('.Addbtn').classList.remove("hide");
+            
+        }
+        else{
+            setData(originalData)
+            document.querySelector('.editbtn').innerHTML = "Edit";
+            document.querySelector('.Addbtn').classList.add("hide");
+        }
+    }
+
+    const onchange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value })
+    }
+
+    const submitHandler=(e)=>{
+        e.preventDefault();
+    }
     return (
         <>
             <form className='form'>
-                <input disabled={'true'} className='inp1' type="text" value={"Pranav Gaur"} />
+                <input disabled={disable} className="inp1 noborder" type="text" name="name" value={data.name} onChange={onchange} />
 
                 <label >
                     <p>Email:</p>
-                    <input disabled={'true'} className='inp2' type="email" value={"gasjdaj@gmail.com"} />
+                    <input disabled={disable} className="inp2 noborder" type="email" name="email" value={data.email} onChange={onchange} />
                 </label>
                 <label >
                     <p>Phone:</p>
-                    <input disabled={'true'} className='inp3' type="number" value={67217313983} />
+                    <input disabled={disable} className="inp3 noborder" type="number" name="phone" value={data.phone} onChange={onchange} />
                 </label>
                 <div className="btns">
 
-                    <button type="submit" className="Addbtn hide">Add</button>
-                    <button className='editbtn'>Edit</button>
+                    <button type="submit" className="Addbtn hide" onClick={submitHandler}>Add</button>
+                    <button className="editbtn" onClick={onEdit}>Edit</button>
                 </div>
             </form>
 
