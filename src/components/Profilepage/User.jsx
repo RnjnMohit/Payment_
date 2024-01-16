eaca3e8cd2943d6aeb16013938caa68b842b196c
PranavGaur7 <raghavgau7@gmail.com>
2024-01-16 18:46:11 +0530

    profile page btns added

--- src/components/Profilepage/User.jsx
+++ src/components/Profilepage/User.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import './user.css'
 const User = () => {
-  return (
-    <>
-        <form className='form'>
-            <input disabled={'true'} className='inp1' type="text" value={"Pranav Gaur"}/>
+    return (
+        <>
+            <form className='form'>
+                <input disabled={'true'} className='inp1' type="text" value={"Pranav Gaur"} />
 
-            <label >
-                <p>Email:</p>
-                <input disabled={'true'} className='inp2' type="email" value={"gasjdaj@gmail.com"} />
-            </label>
-            <label >
-                <p>Phone:</p>
-                <input disabled={'true'} className='inp3' type="number" value={67217313983}/>
-            </label>
-            <button>Add</button>
-        </form>
-        <button>edit</button>
+                <label >
+                    <p>Email:</p>
+                    <input disabled={'true'} className='inp2' type="email" value={"gasjdaj@gmail.com"} />
+                </label>
+                <label >
+                    <p>Phone:</p>
+                    <input disabled={'true'} className='inp3' type="number" value={67217313983} />
+                </label>
+                <div className="btns">
 
-    </>
-  )
+                    <button type="submit" className="Addbtn hide">Add</button>
+                    <button className='editbtn'>Edit</button>
+                </div>
+            </form>
+
+        </>
+    )
 }
 
 export default User
