d9d252d36b408bde2ffbd63af16dc19ce00df9a1
MohitRanjan2433 <mohitranjan11082@gmail.com>
2024-01-24 12:33:01 +0530

    update all

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,29 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './wallet.css';
 import { FaWallet } from "react-icons/fa";
 import { SiBitcoinsv } from "react-icons/si";
 
 const Wallet = () => {
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        // Use a more secure method to get the JWT token (e.g., from HttpOnly cookie)
+        const token = document.cookie.split('=')[1];
+
+        const response = await fetch("http://localhost:3000/account/updateAccount", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        console.log(response);
+        const accountData = await response.json();
+        setBalance(accountData.balance);
+        console.log(accountData);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+
+    fetchBalance();
+  }, []);
+
   return (
-    <div class='big-box'>
-      <div class="wallet">
-        <h1 class="h1">Wallet</h1>
+    <div className='big-box'>
+      <div className="wallet">
+        <h1 className="h1">Wallet</h1>
         <FaWallet className='wallet-icon' />
       </div>
-      <div class="box">
-        <div class="paisa">
+      <div className="box">
+        <div className="paisa">
           <p id='money-ref'>Available Amount:</p>
-          <p id='money'>$500<br /></p>
+          <p id='money'>${balance}<br /></p>
           <p id='name'>Mohit Ranjan</p>
+          <button id='money-add'>Add To Wallet</button>
         </div>
-        <div class="total-trans">
+        <div className="total-trans">
           <p id='trans'>Total Transaction:<br />20<br /><SiBitcoinsv className='price-icon'/></p> 
           <p id='send'>10<br />Send</p>
           <p id='recieved'>10<br />Received</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Wallet;
